d906e5f04aa1e1b758d21f265328842692dfd4d1
alex l <alexliao365@gmail.com>
2024-06-05 22:48:54 -0700

    working class name changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'notification_controller.dart';
-void main() async{
+
+void main() async {
   await AwesomeNotifications().initialize(null, [
-      NotificationChannel(
-        channelKey: 'basic_channel',
-        channelName: 'Basic notifications',
-        channelDescription: 'Basic notifications channel',
-        defaultColor: Color(0xFF9D50DD),
-        ledColor: Colors.white,
-        channelGroupKey: "basic_channel_group"
-      )
-    ],
-    channelGroups: [
-      NotificationChannelGroup(
-        channelGroupKey: 'basic_channel_group',
-        channelGroupName: 'Basic Group',
-      )
-    ]);
-    bool isAllowedToSendNotification = await AwesomeNotifications().isNotificationAllowed();
-    if (!isAllowedToSendNotification) {
-      AwesomeNotifications().requestPermissionToSendNotifications();
-    }
+    NotificationChannel(
+      channelKey: 'basic_channel',
+      channelName: 'Basic notifications',
+      channelDescription: 'Basic notifications channel',
+      defaultColor: Color(0xFF9D50DD),
+      ledColor: Colors.white,
+      channelGroupKey: "basic_channel_group"
+    )
+  ],
+  channelGroups: [
+    NotificationChannelGroup(
+      channelGroupKey: 'basic_channel_group',
+      channelGroupName: 'Basic Group',
+    )
+  ]);
+
+  bool isAllowedToSendNotification = await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowedToSendNotification) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
   runApp(const BellScheduleApp());
 }
 
@@ -35,9 +37,9 @@ class BellScheduleApp extends StatelessWidget {
       title: 'EHS Bell Schedule',
       theme: ThemeData(
         primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.green.shade900, // Dark green background
+        scaffoldBackgroundColor: Colors.green.shade900,
         textTheme: const TextTheme(
-          bodyMedium: TextStyle(color: Colors.white), // White text color for body text
+          bodyMedium: TextStyle(color: Colors.white),
         ),
       ),
       home: const HomePage(),
@@ -57,65 +59,75 @@ class _HomePageState extends State<HomePage> {
   String _currentTime = '';
   String _timeLeft = '';
   String _currentSchedule = '';
-  bool notificationSent = false; // Add this line at the beginning of the _HomePageState class
+  bool notificationSent = false;
+  Map<String, String> customClassNames = {
+    'Period 0': 'Period 0',
+    'Period 1': 'Period 1',
+    'Period 2': 'Period 2',
+    'Period 3': 'Period 3',
+    'Period 4': 'Period 4',
+    'Period 5': 'Period 5',
+    'Period 6': 'Period 6',
+    'Period 7': 'Period 7',
+  };
 
   final Map<String, List<Map<String, String>>> schedules = {
     'Monday': [
-      {'start': '07:15', 'end': '08:20', 'period': '0'},
-      {'start': '08:30', 'end': '09:29', 'period': '1'},
-      {'start': '09:35', 'end': '10:34', 'period': '2'},
-      {'start': '10:40', 'end': '11:39', 'period': '3'},
-      {'start': '11:45', 'end': '12:44', 'period': '4'},
+      {'start': '07:15', 'end': '08:20', 'period': 'Period 0'},
+      {'start': '08:30', 'end': '09:29', 'period': 'Period 1'},
+      {'start': '09:35', 'end': '10:34', 'period': 'Period 2'},
+      {'start': '10:40', 'end': '11:39', 'period': 'Period 3'},
+      {'start': '11:45', 'end': '12:44', 'period': 'Period 4'},
       {'start': '12:44', 'end': '13:14', 'period': 'Lunch'},
-      {'start': '13:20', 'end': '14:19', 'period': '5'},
-      {'start': '14:25', 'end': '15:24', 'period': '6'},
+      {'start': '13:20', 'end': '14:19', 'period': 'Period 5'},
+      {'start': '14:25', 'end': '15:24', 'period': 'Period 6'},
     ],
     'Tuesday': [
-      {'start': '07:15', 'end': '08:20', 'period': '0'},
-      {'start': '08:30', 'end': '09:29', 'period': '1'},
-      {'start': '09:35', 'end': '10:34', 'period': '2'},
-      {'start': '10:40', 'end': '11:39', 'period': '3'},
-      {'start': '11:45', 'end': '12:44', 'period': '4'},
+      {'start': '07:15', 'end': '08:20', 'period': 'Period 0'},
+      {'start': '08:30', 'end': '09:29', 'period': 'Period 1'},
+      {'start': '09:35', 'end': '10:34', 'period': 'Period 2'},
+      {'start': '10:40', 'end': '11:39', 'period': 'Period 3'},
+      {'start': '11:45', 'end': '12:44', 'period': 'Period 4'},
       {'start': '12:44', 'end': '13:14', 'period': 'Lunch'},
-      {'start': '13:20', 'end': '14:19', 'period': '5'},
-      {'start': '14:25', 'end': '15:24', 'period': '6'},
+      {'start': '13:20', 'end': '14:19', 'period': 'Period 5'},
+      {'start': '14:25', 'end': '15:24', 'period': 'Period 6'},
     ],
     'Wednesday': [
       {'start': '08:00', 'end': '09:00', 'period': 'Staff Collaboration'},
-      {'start': '09:00', 'end': '10:30', 'period': '1'},
-      {'start': '10:36', 'end': '12:06', 'period': '3'},
+      {'start': '09:00', 'end': '10:30', 'period': 'Period 1'},
+      {'start': '10:36', 'end': '12:06', 'period': 'Period 3'},
       {'start': '12:06', 'end': '12:36', 'period': 'Lunch'},
       {'start': '12:42', 'end': '13:37', 'period': 'Access'},
-      {'start': '13:43', 'end': '15:13', 'period': '5'},
-      {'start': '15:19', 'end': '22:13', 'period': '10'},
+      {'start': '13:43', 'end': '15:13', 'period': 'Period 5'},
+      {'start': '15:19', 'end': '22:50', 'period': 'Period 7'},
     ],
     'Thursday': [
-      {'start': '07:15', 'end': '08:20', 'period': '0'},
-      {'start': '08:30', 'end': '10:00', 'period': '2'},
-      {'start': '10:06', 'end': '11:36', 'period': '4'},
+      {'start': '07:15', 'end': '08:20', 'period': 'Period 0'},
+      {'start': '08:30', 'end': '10:00', 'period': 'Period 2'},
+      {'start': '10:06', 'end': '11:36', 'period': 'Period 4'},
       {'start': '11:36', 'end': '12:06', 'period': 'Lunch'},
       {'start': '12:12', 'end': '12:57', 'period': 'Access'},
-      {'start': '13:03', 'end': '14:33', 'period': '6'},
+      {'start': '13:03', 'end': '14:33', 'period': 'Period 6'},
     ],
     'Friday': [
-      {'start': '07:15', 'end': '08:20', 'period': '0'},
-      {'start': '08:30', 'end': '09:29', 'period': '1'},
-      {'start': '09:35', 'end': '10:34', 'period': '2'},
-      {'start': '10:40', 'end': '11:39', 'period': '3'},
-      {'start': '11:45', 'end': '12:44', 'period': '4'},
+      {'start': '07:15', 'end': '08:20', 'period': 'Period 0'},
+      {'start': '08:30', 'end': '09:29', 'period': 'Period 1'},
+      {'start': '09:35', 'end': '10:34', 'period': 'Period 2'},
+      {'start': '10:40', 'end': '11:39', 'period': 'Period 3'},
+      {'start': '11:45', 'end': '12:44', 'period': 'Period 4'},
       {'start': '12:44', 'end': '13:14', 'period': 'Lunch'},
-      {'start': '13:20', 'end': '14:19', 'period': '5'},
-      {'start': '14:25', 'end': '15:24', 'period': '6'},
+      {'start': '13:20', 'end': '14:19', 'period': 'Period 5'},
+      {'start': '14:25', 'end': '15:24', 'period': 'Period 6'},
     ],
     'Minimum Day': [
-      {'start': '07:15', 'end': '08:20', 'period': '0'},
-      {'start': '08:30', 'end': '09:05', 'period': '1'},
-      {'start': '09:11', 'end': '09:46', 'period': '2'},
-      {'start': '09:52', 'end': '10:27', 'period': '3'},
-      {'start': '10:33', 'end': '11:08', 'period': '4'},
+      {'start': '07:15', 'end': '08:20', 'period': 'Period 0'},
+      {'start': '08:30', 'end': '09:05', 'period': 'Period 1'},
+      {'start': '09:11', 'end': '09:46', 'period': 'Period 2'},
+      {'start': '09:52', 'end': '10:27', 'period': 'Period 3'},
+      {'start': '10:33', 'end': '11:08', 'period': 'Period 4'},
       {'start': '11:08', 'end': '11:18', 'period': 'Brunch'},
-      {'start': '11:24', 'end': '11:59', 'period': '5'},
-      {'start': '12:05', 'end': '12:40', 'period': '6'},
+      {'start': '11:24', 'end': '11:59', 'period': 'Period 5'},
+      {'start': '12:05', 'end': '12:40', 'period': 'Period 6'},
     ]
   };
 
@@ -131,8 +143,6 @@ class _HomePageState extends State<HomePage> {
     Timer.periodic(const Duration(seconds: 1), (Timer t) => _updateTimeAndClass());
   }
 
-    
-
   void _updateTimeAndClass() {
     DateTime now = DateTime.now();
     String formattedTime =
@@ -143,7 +153,7 @@ class _HomePageState extends State<HomePage> {
 
     String currentClass = 'No Class';
     String timeLeft = '';
-    DateTime? notificationTime; // Nullable DateTime
+    DateTime? notificationTime;
 
     for (var period in schedule) {
       DateTime start = DateTime(now.year, now.month, now.day,
@@ -154,9 +164,9 @@ class _HomePageState extends State<HomePage> {
           int.parse(period['end']!.split(':')[1]));
 
       if (now.isAfter(start) && now.isBefore(end)) {
-        currentClass = 'Period ${period['period']}';
+        currentClass = customClassNames[period['period']!] ?? period['period']!;
         timeLeft = _formatDuration(end.difference(now));
-        notificationTime = end.subtract(Duration(minutes: 2)); // Two minutes before the end of the class
+        notificationTime = end.subtract(Duration(minutes: 2));
         break;
       }
     }
@@ -170,7 +180,6 @@ class _HomePageState extends State<HomePage> {
       _currentSchedule = currentSchedule;
     });
 
-    // Check if it's time to send the notification and if it hasn't been sent already
     if (notificationTime != null && now.isAfter(notificationTime) && !notificationSent) {
       AwesomeNotifications().createNotification(
         content: NotificationContent(
@@ -180,12 +189,9 @@ class _HomePageState extends State<HomePage> {
           body: '$_currentClass ends in 2 minutes!',
         ),
       );
-      notificationSent = true; // Set the flag to true to indicate that the notification has been sent
+      notificationSent = true;
     }
   }
-
-
-
 
   String _getDayOfWeek(DateTime now) {
     switch (now.weekday) {
@@ -234,75 +240,169 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        'EHS Bell Schedule',
-        style: TextStyle(color: Colors.white), // White title color
+  void _openSettings() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SettingsPage(customClassNames: customClassNames),
       ),
-      backgroundColor: const Color(0xFF004d00), // Dark green background
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start, // Align items to the start of the column
-        crossAxisAlignment: CrossAxisAlignment.center, // Center the items horizontally
-        children: <Widget>[
-          const SizedBox(height: 120),
-          if (_currentSchedule != 'No schedule') // Check if there's a schedule
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _currentSchedule,
-                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-            ),
-          const SizedBox(height: 40), // Increased space between schedule and time
-          const Text(
-            'Current Time:',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
-          Text(
-            _currentTime,
-            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          //const Text(
-          //  'Current Class:',
-          //  style: TextStyle(fontSize: 20, color: Colors.white),
-          //),
-          Text(
-            _currentClass,
-            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          if (_currentClass != 'No Class')
-            const Text(
-              'Time Left:',
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-          if (_currentClass != 'No Class')
-            Text(
-              _timeLeft,
-              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-          ElevatedButton(
-            onPressed: () {
-              AwesomeNotifications().createNotification(
-                content: NotificationContent(
-                  id: 1,
-                  channelKey: 'basic_channel',
-                  title: 'EHS Bell Schedule',
-                  body: 'Test notification!',
-                ),
-              );
-            },
-            child: const Text('Test Notification'),
+    );
+    if (result != null) {
+      setState(() {
+        customClassNames = result;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'EHS Bell Schedule',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF004d00),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.white),
+            onPressed: _openSettings,
           ),
         ],
       ),
-    ),
-  );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 120),
+            if (_currentSchedule != 'No schedule')
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  _currentSchedule,
+                  style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+            const SizedBox(height: 40),
+            const Text(
+              'Current Time:',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Text(
+              _currentTime,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _currentClass,
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            if (_currentClass != 'No Class')
+              const Text(
+                'Time Left:',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            if (_currentClass != 'No Class')
+              Text(
+                _timeLeft,
+                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ElevatedButton(
+              onPressed: () {
+                AwesomeNotifications().createNotification(
+                  content: NotificationContent(
+                    id: 1,
+                    channelKey: 'basic_channel',
+                    title: 'EHS Bell Schedule',
+                    body: 'Test notification!',
+                  ),
+                );
+              },
+              child: const Text('Test Notification'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+class SettingsPage extends StatefulWidget {
+  final Map<String, String> customClassNames;
+
+  SettingsPage({required this.customClassNames});
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  late Map<String, TextEditingController> _controllers;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = {
+      for (var period in widget.customClassNames.keys)
+        period: TextEditingController(text: widget.customClassNames[period]),
+    };
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers.values) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings', style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color(0xFF004d00),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          const Text(
+            'Edit your class names:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          ..._controllers.keys.map((period) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextField(
+                controller: _controllers[period],
+                decoration: InputDecoration(
+                  labelText: period,
+                  labelStyle: const TextStyle(color: Colors.white),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 39, 59, 40).withOpacity(0.3),
+                  border: OutlineInputBorder(),
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+            );
+          }).toList(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Map<String, String> updatedNames = {
+            for (var period in _controllers.keys)
+              period: _controllers[period]!.text,
+          };
+          Navigator.pop(context, updatedNames);
+        },
+        child: const Icon(Icons.save),
+        backgroundColor: Color.fromARGB(255, 107, 242, 107),
+      ),
+    );
+  }
 }
